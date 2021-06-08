f6e834b95215f16de3b2693d3545d6c5f37a7cc9
Erez <erezfree29@gmail.com>
2021-06-08 10:19:26 -0700

    fix js linters

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let myLibrary = [];
+const myLibrary = [];
 
-function addRemoveButton () {
-  let remove = document.querySelector('.remove');
-  let tr_d = document.createElement('tr');
-  let td_d = document.createElement('td');
-  let button = document.createElement('button');
-  button.textContent = 'remove'
-  button.className = 'remove_button'
-  let val = document.createAttribute('value');
+function addRemoveButton() {
+  const remove = document.querySelector('.remove');
+  const trd = document.createElement('tr');
+  let tdd = document.createElement('td');
+  const button = document.createElement('button');
+  button.textContent = 'remove';
+  button.className = 'remove_button';
   button.setAttribute('id',myLibrary[myLibrary.length - 1].title);
-  button.setAttribute('onclick',"removeFunction('" + myLibrary[myLibrary.length - 1].title + "')");
-  td_d.appendChild(button);
-  tr_d.appendChild(td_d);
-  remove.appendChild(tr_d);
+  button.setAttribute('onclick', "removeFunction('" + myLibrary[myLibrary.length - 1].title + "')");
+  tdd.appendChild(button);
+  trd.appendChild(tdd);
+  remove.appendChild(trd);
   }
   
-function displayBook () {
+function displayBook() {
   const table = document.querySelector('table');
   const tr = document.createElement('tr');
   const title = document.createElement('td');
@@ -26,11 +25,11 @@ function displayBook () {
   author.textContent = myLibrary[myLibrary.length - 1].author;
   pages.textContent = myLibrary[myLibrary.length - 1].pages;
   read.textContent = myLibrary[myLibrary.length - 1].read;
-  let update_button = document.createElement('button');
+  const updateButton = document.createElement('button');
   read.setAttribute('id',myLibrary[myLibrary.length - 1].title + "read");
-  update_button.setAttribute('onclick',"updateStatus('" + myLibrary[myLibrary.length - 1].title + "')");
-  update_button.textContent = 'update';
-  read.appendChild(update_button);
+  updateButton.setAttribute('onclick', "updateStatus('" + myLibrary[myLibrary.length - 1].title + "')");
+  updateButton.textContent = 'update';
+  read.appendChild(updateButton);
   tr.className = (title.textContent);
   tr.appendChild(title);
   tr.appendChild(author);
@@ -41,38 +40,38 @@ function displayBook () {
 }
 
 const addBookButton = document.querySelector('button');
-addBookButton.onclick = function() {
-  const book = {
-  title: prompt('Enter title'),
-  author: prompt('Enter Author'),
-  pages: prompt('Number of pages'),
+addBookButton.onclick = function () {
+    const book = {
+    title: prompt('Enter title'),
+    author: prompt('Enter Author'),
+    pages: prompt('Number of pages'),
   }
-  let read = prompt('read?').toLocaleLowerCase();
-  while (read != 'yes' && read != 'no') {
-    read = prompt('read?').toLocaleLowerCase();
-  }
+    let read = prompt('read?').toLocaleLowerCase();
+    while (read !== 'yes' && read !== 'no') {
+      read = prompt('read?').toLocaleLowerCase();
+    }
   book.read = read;
   myLibrary.push(book);
   displayBook();
 }
 
-function removeFunction (val) {
-  for(var i in myLibrary){
-    if(myLibrary[i].title==val){
+function removeFunction(val) {
+  for (var i in myLibrary) {
+    if (myLibrary[i].title==val){
       myLibrary.splice(i,1);
-      let rbook = document.querySelector("." + CSS.escape(val) + "");
+      const rbook = document.querySelector("." + CSS.escape(val) + "");
       let rbt = document.querySelector("#" + CSS.escape(val) + "");
       rbook.parentNode.removeChild(rbook);
       rbt.parentNode.removeChild(rbt);
-        break;
+      break;
     }
   }   
 }
 
-function updateStatus (val) {
-  for(var i in myLibrary){
-    if(myLibrary[i].title==val){
-      let read = document.querySelector("#" + CSS.escape(val) + "read" + "");
+function updateStatus(val) {
+  for (let i in myLibrary) {
+    if (myLibrary[i].title === val) {
+      const read = document.querySelector("#" + CSS.escape(val) + "read" + "");
       if (read.firstChild.textContent === 'yes') {
         read.firstChild.textContent = 'no';
         myLibrary[i].read = 'no';
@@ -80,9 +79,7 @@ function updateStatus (val) {
         read.firstChild.textContent = 'yes';
         myLibrary[i].read = 'no';
       }
-        break;
+      break;
     }
   }   
 }
-
-
