f0318fd7c7c0ec2db01a95b55043ecf8b79ca830
Erez <erezfree29@gmail.com>
2021-06-08 08:24:40 -0700

    js linters

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-myLibrary = [];
+let myLibrary = [];
+
 function book(title,author,pages,read) {
   let book = {
-    title : title,
-    author: author,
-    pages : pages,
-    read : read
+    title: title, 
+    author: author, 
+    pages: pages, 
+    read: read
   }
-  return book
+  return book;
 }
 
-function displayBook() {
-  let table = document.querySelector("table");
-  let tr = document.createElement('tr');
-  let title = document.createElement('td');
-  let author = document.createElement('td');
-  let pages = document.createElement('td');
-  let read = document.createElement('td');
+function addRemoveButton () {
+  let remove = document.querySelector('.remove');
+  let tr_d = document.createElement('tr');
+  let td_d = document.createElement('td');
+  let button = document.createElement('button');
+  button.textContent = 'remove'
+  button.className = 'remove_button'
+  let val = document.createAttribute('value');
+  button.setAttribute('id',myLibrary[myLibrary.length - 1].title);
+  button.setAttribute('onclick',"removeFunction('" + myLibrary[myLibrary.length - 1].title + "')");
+  td_d.appendChild(button);
+  tr_d.appendChild(td_d);
+  remove.appendChild(tr_d);
+  }
+  
+function displayBook () {
+  const table = document.querySelector('table');
+  const tr = document.createElement('tr');
+  const title = document.createElement('td');
+  const author = document.createElement('td');
+  const pages = document.createElement('td');
+  const read = document.createElement('td');
   title.textContent = myLibrary[myLibrary.length - 1].title;
   author.textContent = myLibrary[myLibrary.length - 1].author;
   pages.textContent = myLibrary[myLibrary.length - 1].pages;
@@ -29,10 +45,9 @@ function displayBook() {
   addRemoveButton();
 }
 
-
-let addBookButton = document.querySelector('button');
+const addBookButton = document.querySelector('button');
 addBookButton.onclick = function() {
-  let book = {
+  const book = {
   title: prompt('Enter title'),
   author: prompt('Enter Author'),
   pages: prompt('Number of pages'),
@@ -41,22 +56,6 @@ addBookButton.onclick = function() {
   myLibrary.push(book);
   displayBook();
 }
-
-function addRemoveButton () {
-let remove = document.querySelector(".remove");
-let tr_d = document.createElement('tr');
-let td_d = document.createElement('td');
-let button = document.createElement('button');
-button.textContent = 'remove'
-button.className = 'remove_button'
-let val = document.createAttribute('value');
-button.setAttribute('id',myLibrary[myLibrary.length - 1].title);
-button.setAttribute('onclick',"removeFunction('" + myLibrary[myLibrary.length - 1].title + "')");
-td_d.appendChild(button);
-tr_d.appendChild(td_d);
-remove.appendChild(tr_d);
-}
-
 
 function removeFunction (val) {
   for(var i in myLibrary){
@@ -69,7 +68,6 @@ function removeFunction (val) {
         break;
     }
   }   
-  alert(rb);
 }
 
 
